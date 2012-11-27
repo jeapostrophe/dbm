4ccc203b95ae55cea1d079ebe46ea84b053847c6
Jay McCarthy <jay.mccarthy@gmail.com>
2012-11-27 10:56:56 -0700

    don't recreate on every open

--- file/dbm/dbm.rkt
+++ file/dbm/dbm.rkt
@@ -8,14 +8,17 @@
   (define O_RDWR 2)
   (define O_CREAT 512)
   (define O_SYMLINK 2097152)
-  ; XXX Do flags right
+  ;; XXX Do flags right
   (define the-db
-    (dbm_open pth 
-              (bitwise-ior O_RDWR O_CREAT O_SYMLINK)
-              432))
+    (or (dbm_open pth
+                  (bitwise-ior O_RDWR O_SYMLINK)
+                  432)
+        (dbm_open pth
+                  (bitwise-ior O_RDWR O_CREAT O_SYMLINK)
+                  432)))
   (if the-db
-      (dbm the-db)
-      (error 'dbm-open "Could not open DBM: ~e" pth)))
+    (dbm the-db)
+    (error 'dbm-open "Could not open DBM: ~e" pth)))
 
 (define (dbm-close! dbm)
   (unless (zero? (dbm_close (dbm-ptr dbm)))
@@ -51,14 +54,14 @@
   (match (dbm_fetch (dbm-ptr dbm) (string->datum key))
     [#f
      (if (procedure? fail)
-         (fail)
-         fail)]
-    [(? datum? d) 
+       (fail)
+       fail)]
+    [(? datum? d)
      (match (datum->string d)
        [#f
         (if (procedure? fail)
-            (fail)
-            fail)]
+          (fail)
+          fail)]
        [(? string? v)
         v])]))
 
@@ -72,8 +75,8 @@
 (define (dbm-count dbm)
   (let loop ([i 0] [first? #f])
     (if (if first? (dbm-next-key dbm) (dbm-first-key dbm))
-        (loop (add1 i) #t)
-        i)))
+      (loop (add1 i) #t)
+      i)))
 
 (define (dbm-iterate-first dbm)
   (define f-key (dbm-first-key dbm))
@@ -87,18 +90,18 @@
 (define (dbm-iterate-value dbm pos) (cdr pos))
 
 (struct dbm (ptr)
-  #:property prop:dict
-  (vector dbm-ref
-          (lambda (dbm key val)
-            (dbm-set! dbm key val #:replace? #t))
-          #f
-          dbm-remove!
-          #f
-          dbm-count
-          dbm-iterate-first
-          dbm-iterate-next
-          dbm-iterate-key
-          dbm-iterate-value))
+        #:property prop:dict
+        (vector dbm-ref
+                (lambda (dbm key val)
+                  (dbm-set! dbm key val #:replace? #t))
+                #f
+                dbm-remove!
+                #f
+                dbm-count
+                dbm-iterate-first
+                dbm-iterate-next
+                dbm-iterate-key
+                dbm-iterate-value))
 
 (provide/contract
  [dbm? (any/c . -> . boolean?)]
